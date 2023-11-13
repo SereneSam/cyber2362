5b08e4889f23fcdf4651fb51e313c5a79ef422b3
Chris M <cmccol6@lsu.edu>
2023-11-13 16:15:17 -0600

    made a bunch of changes

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
-var express = require('express');
-var app = express();
-const ldap = require('ldapjs');
+const express = require("express");
+const ldap = require("ldapjs");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-app.listen(3000, function () {
-    console.log("Server started")
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname)));
 
-function authenticationDN(username, password) {
+// Serve HTML login page
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/loginPage.html");
+});
 
-}
+app.get("/filesPage", (req, res) => {
+  res.sendFile(path.join(__dirname + "/filesPage.html"));
+});
+
+app.get("/loginPage", (req, res) => {
+  res.sendFile(path.join(__dirname + "/loginPage.html"));
+});
+
+// Handle login form submission
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // LDAP authentication logic
+  const client = ldap.createClient({
+    url: "ldap://172.18.160.1:10389",
+  });
+
+  client.bind(`cn=${username},ou=users,ou=system`, password, (err) => {
+    if (err) {
+      res.send("Login failed. Check your username and password.");
+    } else {
+      res.redirect("/filesPage");
+    }
+
+    client.unbind();
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
